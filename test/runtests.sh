b5ce971b9ee3fb2e781b33b3883a24a62b90dad1
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-08-23 12:05:17 -0700

    Added stats.js

--- test/runtests.sh
+++ test/runtests.sh
@@ -13,6 +13,7 @@ mocha                                 \
   test/app_test.js                    \
   test/exchanges_test.js              \
   test/publisher_test.js              \
+  test/stats_test.js                  \
   test/testing/localapp_test.js       \
   test/testing/localapp2_test.js      \
   test/testing/schemas_test.js        \
