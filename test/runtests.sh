06d10c39b85861b9cc8d9b8aa81d825dfa5007cd
Dustin J. Mitchell <dustin@mozilla.com>
2015-10-12 08:08:24 -0400

    Bug 1213794: fix test filename

--- test/runtests.sh
+++ test/runtests.sh
@@ -20,7 +20,7 @@ local_tests=(
   test/api/schemaprefix_test.js
   test/api/noncemanager_test.js
   test/app_test.js
-  test/scopematch_test.js
+  test/utils_test.js
   test/exchanges_test.js
   test/testing/localapp_test.js
   test/testing/localapp2_test.js
