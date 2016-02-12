6ddaa4cfa6c34c1915ea24d2d15a7fb85ed99a63
Jonas Finnemann Jensen <jopsen@gmail.com>
2016-02-12 11:45:03 -0800

    bump version

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
-require('./');
+var base = require('./');
+var _ = require('lodash');
+var assert = require('assert');
+_.forIn(base, function(module, name) {
+  console.log('Loading: ' + name);
+   assert(module, 'Failed to load: ' + name);
+});
 console.log('WOOHOO! All of the modules import correctly.');
 console.log('This is the only test we need for this package.');
